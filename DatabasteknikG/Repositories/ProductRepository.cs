8c182c353e69e97d576d977d3589322975471d3d
Angelo Yousef <angelo.yousef@yh.nackademin.se>
2023-11-19 23:18:51 +0100

    first commit

--- DatabasteknikG/Repositories/ProductRepository.cs
+++ DatabasteknikG/Repositories/ProductRepository.cs
@@ -36,44 +36,22 @@ namespace DatabasteknikG.Repositories
                 existingProduct.ProductDescription = entity.ProductDescription;
                 existingProduct.ProductPrice = entity.ProductPrice;
 
-                // Update or create PricingUnit
-                if (entity.PricingUnit != null)
+                if (existingProduct.PricingUnit != null)
                 {
-                    if (existingProduct.PricingUnit != null)
-                    {
-                        // Update existing PricingUnit
-                        existingProduct.PricingUnit.Unit = entity.PricingUnit.Unit;
-                    }
-                    else
-                    {
-                        // Create new PricingUnit and associate with the product
-                        existingProduct.PricingUnit = new PricingUnitEntity { Unit = entity.PricingUnit.Unit };
-                    }
+                    existingProduct.PricingUnit.Unit = entity.PricingUnit?.Unit;
                 }
                 else
                 {
-                    // If entity.PricingUnit is null, you may decide whether to clear the association or leave it unchanged.
-                    // For example, you can set existingProduct.PricingUnit = null;
+                    existingProduct.PricingUnit = entity.PricingUnit;
                 }
 
-                // Update or create ProductCategory
-                if (entity.ProductCategory != null)
+                if (existingProduct.ProductCategory != null)
                 {
-                    if (existingProduct.ProductCategory != null)
-                    {
-                        // Update existing ProductCategory
-                        existingProduct.ProductCategory.CategoryName = entity.ProductCategory.CategoryName;
-                    }
-                    else
-                    {
-                        // Create new ProductCategory and associate with the product
-                        existingProduct.ProductCategory = new ProductCategoryEntity { CategoryName = entity.ProductCategory.CategoryName };
-                    }
+                    existingProduct.ProductCategory.CategoryName = entity.ProductCategory?.CategoryName;
                 }
                 else
                 {
-                    // If entity.ProductCategory is null, you may decide whether to clear the association or leave it unchanged.
-                    // For example, you can set existingProduct.ProductCategory = null;
+                    existingProduct.ProductCategory = entity.ProductCategory;
                 }
 
                 await _context.SaveChangesAsync();
@@ -81,7 +59,6 @@ namespace DatabasteknikG.Repositories
 
             return existingProduct;
         }
-
 
 
 
